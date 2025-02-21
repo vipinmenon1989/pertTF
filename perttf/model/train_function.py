e26640e264cc6e4de6c17601917a8de4f6a391cd
Wei Li <li.david.wei@gmail.com>
2025-02-21 15:28:55 -0800

    fix a bug of perturbation_label type

--- perttf/model/train_function.py
+++ perttf/model/train_function.py
@@ -509,7 +509,7 @@ def eval_testdata(
                 src_key_padding_mask=src_key_padding_mask,
                 batch_size=config.batch_size,
                 batch_labels=torch.from_numpy(batch_ids).long() if config.use_batch_label else None, # if config.DSBN else None,
-                pert_labels = torch.from_numpy(perturbation_labels).long() if config.perturbation_input else None,                                                                     
+                pert_labels = torch.from_numpy(perturbation_indexes).long() if config.perturbation_input else None,                                                                     
                 time_step=0,
                 return_np=True,
             )
