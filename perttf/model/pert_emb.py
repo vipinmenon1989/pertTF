f39b1580fc100c62793dbb4a38e58fc01668acc7
Wei Li <li.david.wei@gmail.com>
2025-04-10 15:30:55 +0000

    fix bug for importing

--- perttf/model/pert_emb.py
+++ perttf/model/pert_emb.py
@@ -3,6 +3,11 @@ import torch
 import os
 from typing import Literal
 
+import scanpy as sc
+import numpy as np
+import pandas as pd
+from perttf.model.train_function import eval_testdata
+
 def load_pert_embedding_from_gears(gears_path, adata, 
                                   intersect_type : Literal["common","gears"] = "common"):
     """
