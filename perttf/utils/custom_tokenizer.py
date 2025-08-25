fix cls value masking and assignment: 1. added cls_value assignment for collator 2. fixed assignment of cls_value in tokenizer and padding functions 3. fixed masking of positions, previous masking would mask the cls position because its value was fixed at zero

perttf/utils/custom_tokenizer.py
@@ -73,7 +73,7 @@ class SimpleVocab:
 
 
 
-# This function remains unchanged
+# added the cls value is append cls (previously it was 0)
 def tokenize_batch(
     data: np.ndarray,
     gene_ids: np.ndarray,
@@ -81,6 +81,7 @@ def tokenize_batch(
     append_cls: bool = True,
     include_zero_gene: bool = False,
     cls_id: int = "<cls>",
+    cls_value: int = -3,
     mod_type: np.ndarray = None,
     cls_id_mod_type: int = None,
 ) -> List[Tuple[Union[torch.Tensor, np.ndarray]]]:
@@ -125,7 +126,7 @@ def tokenize_batch(
                 mod_types = mod_type[idx]
         if append_cls:
             genes = np.insert(genes, 0, cls_id)
-            values = np.insert(values, 0, 0)
+            values = np.insert(values, 0, cls_value)
             if mod_type is not None:
                 mod_types = np.insert(mod_types, 0, cls_id_mod_type)
         if return_pt:
@@ -251,11 +252,12 @@ def tokenize_and_pad_batch(
     gene_ids: np.ndarray,
     max_len: int,
     vocab: Vocab,
-    pad_token: str,
-    pad_value: int,
-    append_cls: bool = True,
     include_zero_gene: bool = False,
+    pad_token: str = "<pad>",
+    pad_value: int = -2,
+    append_cls: bool = True,
     cls_token: str = "<cls>",
+    cls_value: int = -3,
     return_pt: bool = True,
     mod_type: np.ndarray = None,
     vocab_mod: Vocab = None,
@@ -287,6 +289,7 @@ def tokenize_and_pad_batch(
         append_cls=append_cls,
         include_zero_gene=include_zero_gene,
         cls_id=cls_id,
+        cls_value=cls_value,
         mod_type=mod_type,
         cls_id_mod_type=cls_id_mod_type,
     )
@@ -309,6 +312,7 @@ def random_mask_value(
     mask_ratio: float = 0.15,
     mask_value: int = -1,
     pad_value: int = 0,
+    cls_value: int = -3
 ) -> torch.Tensor:
     """
     Randomly mask a batch of data.
@@ -331,7 +335,7 @@ def random_mask_value(
 
     for i in range(len(values)):
         row = values[i]
-        non_padding_idx = np.nonzero(row - pad_value)[0]
+        non_padding_idx = np.intersect1d(np.nonzero(row - pad_value), np.nonzero(row - cls_value)) # only mask non padding or cls positions
         n_mask = int(len(non_padding_idx) * mask_ratio)
         mask_idx = np.random.choice(non_padding_idx, n_mask, replace=False)
         row[mask_idx] = mask_value

perttf/utils/pert_data_loader.py
@@ -231,6 +231,8 @@ class PertBatchCollator:
         self.full_tokenize = full_tokenize
         self.append_cls = config.get('append_cls', True)
         self.include_zero_gene = config.get('include_zero_gene', True)
+        self.append_cls = config.get('append_cls', True)
+        self.cls_value = config.get('cls_value', -3)
         self.max_seq_len = config.get('max_seq_len', 3000)
         self.pad_token = config.get('pad_token', '<pad>')
         self.pad_value = config.get('pad_value', -2)
@@ -252,22 +254,25 @@ class PertBatchCollator:
         # during validation and predictions, this window may be around all genes with expression
         max_seq_len = self.max_seq_len if not self.full_tokenize else len(self.gene_ids) + self.append_cls
 
-        # TODO: These functions may need to be modified to accomodate inputs w differing number of genes
+        # TODO: These functions may need to be modified to accomodate inputs w differing number of genes in the future
         tokenized, gene_idx_list = tokenize_and_pad_batch(
             np.array(expr_list), self.gene_ids, max_len=max_seq_len,
             vocab=self.vocab, pad_token=self.pad_token, pad_value=self.pad_value,
-            append_cls=self.append_cls, include_zero_gene=self.include_zero_gene
+            append_cls=self.append_cls, include_zero_gene=self.include_zero_gene, 
+            cls_value=self.cls_value
         )
         tokenized_next, _ = tokenize_and_pad_batch(
             np.array(expr_next_list), self.gene_ids, max_len=max_seq_len,
             vocab=self.vocab, pad_token=self.pad_token, pad_value=self.pad_value,
-            append_cls=self.append_cls, include_zero_gene=self.include_zero_gene, sample_indices=gene_idx_list
+            append_cls=self.append_cls, include_zero_gene=self.include_zero_gene, 
+            sample_indices=gene_idx_list, cls_value=self.cls_value
         )
         
         # 3. Apply random masking for this batch
         masked_values = random_mask_value(
             tokenized["values"], mask_ratio=self.mask_ratio,
             mask_value=self.mask_value, pad_value=self.pad_value,
+            cls_value= self.cls_value
         )
 
         # 4. Collate all other labels into tensors